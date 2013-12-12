9355f56574eee4b91f148c2e48d989dc0a61e20f
ben <ben@dreamerslab.com>
2013-12-12 13:21:55 -0800

    update example

--- example/app.js
+++ example/app.js
@@ -1,24 +1,35 @@
-var express = require('express')
-  , engine = require('../')
-  , app = express();
+/**
+ * Module dependencies.
+ */
 
-// use ejs-locals for all ejs templates:
-app.engine('ejs', engine);
+var express = require( 'express' );
+var routes  = require( './routes' );
+var http    = require( 'http' );
+var path    = require( 'path' );
+var engine  = require( '../index.js' );
 
-app.set('views',__dirname + '/views');
-app.set('view engine', 'ejs'); // so you can render('index')
+var app = express();
 
-// render 'index' into 'boilerplate':
-app.get('/',function(req,res,next){
-  res.render('index', { what: 'best', who: 'me', muppets: [ 'Kermit', 'Fozzie', 'Gonzo' ] });
+// all environments
+app.engine( 'html', engine );
+app.set( 'view engine', 'html' );
+app.set( 'views', path.join( __dirname, 'views' ));
+app.set( 'port', process.env.PORT || 3000 );
+app.use( express.favicon());
+app.use( express.logger( 'dev' ));
+app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.methodOverride());
+app.use( app.router );
+app.use( express.static( path.join( __dirname, 'public' )));
+
+// development only
+if( 'development' == app.get( 'env' )){
+  app.use( express.errorHandler());
+}
+
+app.get( '/', routes.index );
+
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
 });
-
-app.get('/foo.js', function(req,res,next){
-	res.sendfile('foo.js');
-})
-
-app.get('/foo.css', function(req,res,next){
-	res.sendfile('foo.css');
-})
-
-app.listen(3000);
